94a379e565ca14cce864046e834afa420184934d
EthanSk13s <ethanmiranda11@gmail.com>
2021-08-10 23:22:18 -0700

    change ips and ports

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from mirion import create_app
 
 if __name__ == "__main__":
     app = create_app('config.Config')
-    app.run(host='127.0.0.1', port=5500, debug=True)
+    app.run(host='0.0.0.0', port=5500, debug=True)
