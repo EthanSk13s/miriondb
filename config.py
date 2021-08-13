b8cb7dbbad1f2b4edec771773a69e436a556acfa
EthanSk13s <ethanmiranda11@gmail.com>
2021-08-12 18:16:46 -0700

    convert config to postgres

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
 import os
+import yaml
+
 basedir = os.getcwd()
+
+try:
+    with open("db.yml", "r") as f:
+        cfg = yaml.safe_load(f)['postgres']
+        url = f"postgresql://{cfg['user']}:{cfg['pass']}@localhost/{cfg['database']}"
+except FileNotFoundError:
+    print("Config yaml not found, resorting to env vars.")
+    url = os.environ.get('DATABASE_URL')
 
 
 class Config(object):
     SCHEDULER_API_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'theater.db')
+    SQLALCHEMY_DATABASE_URI = url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
