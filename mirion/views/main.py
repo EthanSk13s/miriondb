c19055c734fc33e87fbdc3b989e05039bd329d0b
EthanSk13s <ethanmiranda11@gmail.com>
2021-10-03 17:36:03 -0700

    fix missing cards from row

--- mirion/views/main.py
+++ mirion/views/main.py
@@ -47,6 +47,7 @@ def index():
                 sorted_additions.append([x for x in cards])
                 cards.clear()
         except IndexError:
+            cards.append(card)
             sorted_additions.append([x for x in cards])
 
     return render_template('main.html', recent_additions=recent_additions,
