88c43b7722a2e361f273258f730d86bec076c3d4
EthanSk13s <ethanmiranda11@gmail.com>
2021-10-22 00:41:58 -0700

    add deployment stuff

--- start.sh
+++ start.sh
@@ -6,8 +6,11 @@ case "$CMD" in
         exec python run.py
         ;;
     theater)
-        cd theater
+        cd theater || exit
         exec ./theater
+        ;;
+    deploy)
+        exec gunicorn --workers=1 -b 0.0.0.0:5500 "run:create_app('config.Config')"
         ;;
     *) echo "Did not supply an argument"
         ;;
