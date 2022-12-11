1ffbe2356428077b1cb093afd57a64df6543edb3
EthanSk13s <ethanmiranda11@gmail.com>
2022-12-10 19:11:00 -0800

    add image to not found

--- ui/src/views/NotFoundView.vue
+++ ui/src/views/NotFoundView.vue
@@ -2,5 +2,8 @@
     <main class="flex flex-col justify-center m-auto md:w-7/12 mt-5 gap-2">
         <h1 class="mx-2 md:mx-0 text-white text-4xl">404 Not Found</h1>
         <p class="mx-2 md:mx-0 text-white text-lg">That page does not exist here</p>
+        <div class="mx-2 md:mx-0 animate-wiggle">
+            <img class="rounded-lg" src="/NotFound.png" alt="Not Found">
+        </div>
     </main>
 </template>
--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'wiggle': 'wiggle 0.25s ease-in-out infinite'
+      },
+      keyframes: {
+        wiggle: {
+          '0%': { 'transform': 'translateX(0)' },
+          '25%': { 'transform': 'translateX(0.5px)' },
+          '50%': { 'transform': 'translateX(-0.5px)' },
+          '75%': { 'transform': 'translateX(0.5px)' },
+          '100%': { 'transform': 'translateX(0)' }
+        }
+      }
+    },
   },
   plugins: [],
 };
