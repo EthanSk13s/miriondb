e547a6c42a75cf0a0eb4d4812198fc05c29e5ed4
EthanSk13s <ethanmiranda11@gmail.com>
2022-12-17 14:21:04 -0800

    change stylings

--- ui/src/components/partials/SearchBar.vue
+++ ui/src/components/partials/SearchBar.vue
@@ -37,6 +37,8 @@ export default {
             items.forEach((item) => {
                 item.addEventListener('click', () => {
                     searchBar.value = item.getElementsByTagName('input')[0].value;
+
+                    this.searchString = '';
                 })
             })
         }
@@ -50,8 +52,8 @@ export default {
             class="w-full py-1 pl-2 bg-slate-800 text-white rounded focus:outline-none focus:ring focus:ring-violet-400"
             :placeholder="placeholder">
         <div v-if="searchString"
-            class="top-full inset-x-0 py-1 my-2 bg-slate-800 text-white rounded-lg z-50 shadow-lg absolute">
-            <div ref="items" class="pl-2 py-1 cursor-pointer hover:bg-slate-900" v-for="result in matchResult()">
+            class="top-full inset-x-0 p-1 my-2 bg-slate-800 text-white rounded-lg z-50 drop-shadow-2xl absolute">
+            <div ref="items" class="pl-2 py-1 cursor-pointer hover:bg-slate-900 hover:rounded" v-for="result in matchResult()">
                 <strong>{{ result.substring(0, searchString.length) }}</strong>{{ result.substring(searchString.length) }}
                 <input type="hidden" :value="result">
             </div>
