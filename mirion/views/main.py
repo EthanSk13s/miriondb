3ebe95b572ff3d870ca7ca8886b1c4c64536ff7f
EthanSk13s <ethanmiranda11@gmail.com>
2025-08-31 18:46:35 -0700

    refactor main page api route

--- mirion/views/main.py
+++ mirion/views/main.py
@@ -15,27 +15,41 @@ def resource_not_found(e):
 
 @main_page.route("/latest")
 def latest():
-    recent_datetime = Card.query.filter(Card.ex_type != 13,
-                                        Card.event_id == None,
-                                        Card.idol_type != 5).order_by(Card.id.desc()).first().release
-    recent_additions = Card.query.filter(recent_datetime == Card.release,
-                                         Card.event_id == None, Card.ex_type != 13).order_by(Card.id.asc()).all()
+    recent_datetime =  (
+        Card.query.filter(Card.ex_type != 13,
+                          Card.event_id == None,
+                          Card.idol_type != 5)
+            .order_by(Card.db_id.desc())
+            .first().release
+    )
+    recent_additions = (
+        Card.query.filter(recent_datetime == Card.release,
+                          Card.event_id == None,
+                          Card.ex_type != 13)
+            .order_by(Card.id.asc()).all()
+    )
 
     current_event: Event = Event.query.order_by(-Event.id).first()
     if current_event.event_type in (3, 4, 5, 9, 11, 13, 16):
-        event_cards = Card.query.filter(Card.event_id == current_event.id,
-                                        sqlalchemy.or_(Card.rarity == 3, Card.rarity == 2)).all()
-    else:
-        event_cards = None
+        event_cards = (
+            Card.query.filter(Card.event_id == current_event.id,
+                              sqlalchemy.or_(Card.rarity.in_([3, 2]))).all()
+        )
 
-    previous_dates = Card.query.with_entities(Card.release).filter(recent_datetime != Card.release,
-                                                                   Card.ex_type != 13, Card.event_id == None).\
-        order_by(Card.release.desc()).\
-        group_by(Card.release)[0:2]
+    previous_dates = (
+        Card.query.with_entities(Card.release)
+            .filter(recent_datetime != Card.release,
+                    Card.ex_type != 13,
+                    Card.event_id == None)
+            .order_by(Card.release.desc())
+            .group_by(Card.release)[0:2]
+    )
 
     dates = [card.release for card in previous_dates]
-
-    previous_additions = Card.query.filter(Card.release.in_(dates)).order_by(Card.release.desc()).all()
+    previous_additions = (
+        Card.query.filter(Card.release.in_(dates))
+            .order_by(Card.release.desc()).all()
+    )
 
     sorted_additions = helpers.list_grouper(previous_additions,
                                             helpers.check_for_release)
@@ -44,15 +58,19 @@ def latest():
         'currentEvent': 
             {
                 'event': current_event.serialize,
-                'cards': [card.mini_serialize for card in event_cards] if event_cards is not None else event_cards
             },
         'recentCards': [card.mini_serialize for card in recent_additions],
         'previousAdditions': []
     }
 
-    temp_list = []
+    if event_cards:
+        payload['currentEvent']['cards'] = [card.mini_serialize for card in event_cards]
+    else:
+        payload['currentEvent']['cards'] = None
+
+    prev_list = []
     for date in sorted_additions:
-        temp_list.append([card.mini_serialize for card in date])
+        prev_list.append([card.mini_serialize for card in date])
 
     payload['previousAdditions'] = temp_list
 
